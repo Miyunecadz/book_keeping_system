a7ba91412161c21b183d5ece78b07b8370d6a692
miyunecadz <jvcadz@gmail.com>
2022-05-14 21:07:44 +0800

    Add migrations for users and customers

--- database/migrations/2022_05_14_130220_create_customers_table.php
+++ database/migrations/2022_05_14_130220_create_customers_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('address');
+            $table->foreignId('users_id')->constrained('user')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
